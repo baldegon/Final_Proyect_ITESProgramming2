2a220f12a14ae8838bf11f6fc3df65716afb4c74
Bladimir Holmann Zink <bladimirzink7@gmail.com>
2024-10-21 16:51:14 -0300

    Adicion de supabase al proyecto y primera query

--- db/config.ts
+++ db/config.ts
@@ -1,4 +1,15 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
+
+const Empleados = {
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    surname: column.text(),
+    age: column.number(),
+
+  }
+}
+
 
 // https://astro.build/db/config
 export default defineDb({
