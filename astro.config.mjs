e7722e6981cd6aefa25287f47a45621ef04dc4c7
Bladimir Holmann Zink <bladimirzink7@gmail.com>
2024-10-22 16:12:31 -0300

    añadiendo auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+export const prerender = false;
 
 // https://astro.build/config
 export default defineConfig({
+  output: hybrid,
   integrations: [tailwind()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+
+
+
+module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			sans: ['Inter, Gotham SSm A, sans-serif'],
+			
+		},
 		extend: {},
 	},
 	plugins: [],
